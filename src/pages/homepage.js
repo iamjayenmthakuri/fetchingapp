5fd38946d3683c9da6d1640d216866f10d98e8a6
iamjayenmthakuri <malla.jayen@gmail.com>
2023-04-23 17:17:31 +0545

    Detail of video description is modified

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import useFetch from "@/components/useFetch";
 import Items from "@/pages/playlists";
 import Router from "next/router";
+import Head from "next/head";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 import styles from "../styles/homepage.module.css";
@@ -52,41 +53,48 @@ export default function Homepage() {
     window.location.href = "/";
   };
   return (
-    <main className={styles.container}>
-      <div className={styles.wrapper}>
-        <div className={styles.heading}>
-          <h1>Welcome to Youtube Playlist</h1>
-          <p>
-            This are the playlist of Mr.Arun Maini, the UK youtuber from his
-            channel MR.whosetheboss.
-          </p>
-          <div className={styles.nav}>
-            <button onClick={handleLogout}>Logout</button>
+    <>
+      <Head>
+        <title>Welcome to the Homepage</title>
+        <meta name="description" content="Login in with our App" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main className={styles.container}>
+        <div className={styles.wrapper}>
+          <div className={styles.heading}>
+            <h1>Welcome to Youtube Playlist</h1>
+            <p>
+              This are the playlist of Mr.Arun Maini, the UK youtuber from his
+              channel MR.whosetheboss.
+            </p>
+            <div className={styles.nav}>
+              <button onClick={handleLogout}>Logout</button>
+            </div>
           </div>
-        </div>
-        {playlistId ? (
-          playlistId && <Items playlistId={playlistId} />
-        ) : (
-          <div className={styles.playlistsWrapper}>
-            {data.map((item) => (
-              <div
-                className={styles.playlists}
-                key={item.id}
-                onClick={() => ViewPlaylist(item.id, item.snippet.title)}
-              >
-                {" "}
-                <div className={styles.playlist}>
-                  <p>{item.snippet.title}</p>
-                  <img
-                    src={item.snippet.thumbnails.high.url}
-                    alt={item.snippet.title}
-                  />
+          {playlistId ? (
+            playlistId && <Items playlistId={playlistId} />
+          ) : (
+            <div className={styles.playlistsWrapper}>
+              {data.map((item) => (
+                <div
+                  className={styles.playlists}
+                  key={item.id}
+                  onClick={() => ViewPlaylist(item.id, item.snippet.title)}
+                >
+                  {" "}
+                  <div className={styles.playlist}>
+                    <p>{item.snippet.title}</p>
+                    <img
+                      src={item.snippet.thumbnails.high.url}
+                      alt={item.snippet.title}
+                    />
+                  </div>
                 </div>
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
-    </main>
+              ))}
+            </div>
+          )}
+        </div>
+      </main>
+    </>
   );
 }
--- src/pages/playlists/index.js
+++ src/pages/playlists/index.js
@@ -1,26 +1,31 @@
 import { useState } from "react";
+import Head from "next/head";
 import { useRouter } from "next/router";
 import useFetch from "@/components/useFetch";
 import styles from "../playlists/playlist.module.css";
 
 const Items = (props) => {
   const router = useRouter();
+  const handlePlaylist = () => {
+    router.push("/homepage");
+  };
 
   const handlePrevious = () => {
     router.push("/homepage");
   };
 
-  const { playlistId, title } = props;
+  const { playlistId, title, id } = props;
   const url = `${process.env.youtubeLink}playlistItems?part=snippet&maxResults=6&playlistId=${playlistId}&key=${process.env.customKey}`;
   const [detail, setDetail] = useState();
+  const [hide, setHide] = useState(false);
   const viewDetail = (id) => {
     if (detail === id) {
       setDetail(null);
     } else {
       setDetail(id);
+      setHide(!hide);
     }
   };
-  const [hide, setHide] = useState(false);
   const { data, isPending, error } = useFetch(url);
 
   if (isPending) {
@@ -43,19 +48,40 @@ const Items = (props) => {
   return (
     <main>
       <div className={styles.wrapper}>
-        <div className={styles.heading}>
-          <h1>{title}</h1>
-          <p>
-            This are the videos of {title}, playlist that you have selected from
-            Home page
-          </p>
-          <div className={styles.nav}>
+        {hide ? (
+          <>
             {" "}
-            <button onClick={handlePrevious} className="button">
-              Back to Main Page
-            </button>
-          </div>
-        </div>
+            <div className={styles.heading}>
+              <h1>{title}</h1>
+              <p>
+                This is the video of {title}, playlist that you have selected
+                from Playlist Page
+              </p>
+              <div className={styles.nav}>
+                {" "}
+                <button onClick={handlePlaylist} className="button">
+                  Back to PLaylist
+                </button>
+              </div>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className={styles.heading}>
+              <h1>{title}</h1>
+              <p>
+                This are the videos of {title}, playlist that you have selected
+                from Home page
+              </p>
+              <div className={styles.nav}>
+                {" "}
+                <button onClick={handlePrevious} className="button">
+                  Back to Main Page
+                </button>
+              </div>
+            </div>
+          </>
+        )}
         <div className={styles.playlistsWrapper}>
           {data.map((item) => (
             <div
@@ -65,24 +91,45 @@ const Items = (props) => {
             >
               <div>
                 {detail !== item?.etag ? (
-                  <div className={styles.playlist}>
-                    <p>{item.snippet.title}</p>
-                    <img
-                      src={item.snippet.thumbnails.high.url}
-                      alt={item.snippet.title}
-                    />
-                  </div>
+                  <>
+                    {!hide && (
+                      <div className={styles.playlist}>
+                        {" "}
+                        <p>{item.snippet.title}</p>
+                        <img
+                          src={item.snippet.thumbnails.high.url}
+                          alt={item.snippet.title}
+                        />
+                      </div>
+                    )}
+                  </>
                 ) : (
                   <>
-                    <div className={styles.play}>
+                    <Head>
+                      <title>{item.snippet.title}</title>
+                      <meta
+                        name="description"
+                        content="Login in with our App"
+                      />
+                      <meta
+                        name="viewport"
+                        content="width=device-width, initial-scale=1"
+                      />
+                    </Head>
+
+                    <div className={styles.videoDetail}>
+                      <h1 className={styles.detailTitle}>
+                        {item.snippet.title}
+                      </h1>
                       <img
                         src={item.snippet.thumbnails.high.url}
                         alt={item.snippet.title}
-                        className={styles.thumbnails}
+                        className={styles.detailImage}
                       />
-                      <h2 className={styles.title}>{item.snippet.title}</h2>
+
                       <h4 className={styles.author}>
-                        Author:{item.snippet.channelTitle}
+                        <span>Author: </span>
+                        {item.snippet.channelTitle}
                       </h4>
                       <p className={styles.publishedDate}>
                         Published Date:{item.snippet.publishedAt}
